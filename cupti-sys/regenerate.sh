ee936a5664c3363bc1fe674357fb5265a44796a4
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2025-11-20 16:01:56 -0800

    Allow regenerate.sh to be called from anywhere

--- cupti-sys/regenerate.sh
+++ cupti-sys/regenerate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
+
 cargo xtask regenerate cupti.cpp    \
     --output src/bindings.rs        \
     --                              \
