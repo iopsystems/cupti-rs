40746192b52fcb899c515868d7b90e9620d4c5cf
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2025-11-24 16:50:13 -0800

    Include missing size constants in bindings

--- cupti-sys/regenerate.sh
+++ cupti-sys/regenerate.sh
@@ -8,6 +8,6 @@ cargo xtask regenerate cupti.cpp    \
     --                              \
     -I /usr/local/cuda/include
 
-grep -EhR '#define (CUpti_[a-zA-Z0-9_]+_STRUCT_SIZE)\s+CUPTI_[A-Za-z0-9_]*STRUCT_SIZE\([A-Za-z_0-9]+,\s+[a-zA-Z0-9]+\)' /usr/local/cuda/include \
-    | sed -E 's/#define (CUpti_[a-zA-Z0-9_]+_STRUCT_SIZE)\s+CUPTI_[A-Z]+_STRUCT_SIZE\(([A-Za-z0-9_]+), ([A-Za-z0-9_]+)\)/pub const \1: usize = cupti_struct_size!(\2, \3);/g' \
+grep -EhR '#define (CUpti_[a-zA-Z0-9_]+)\s+CUPTI_[A-Za-z0-9_]*STRUCT_SIZE\([A-Za-z_0-9]+,\s*[a-zA-Z0-9]+\)' /usr/local/cuda/include \
+    | sed -E 's/#define (CUpti_[a-zA-Z0-9_]+)\s+CUPTI_[A-Z]+_STRUCT_SIZE\(([A-Za-z0-9_]+),\s*([A-Za-z0-9_]+)\)/pub const \1: usize = cupti_struct_size!(\2, \3);/g' \
     > src/sizes.rs
